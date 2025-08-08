f26406363d73b77d79c55bf58f89d507a7bd28ca
ammaraltaf064 <ammaraltaf064@gmail.com>
2025-08-08 13:27:39 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 144552
+let accountEmail= "Ammar@gmail.com"
+var accountPassword="12345"
+accountCity="Lahore"
+let accountState;
+
+// accountId=3//not allowed due to already constant
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])//give the data in table form by using of only variables name
+/* prefer not to use var because of block and functional scope
+ */
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"//treat js as newest version
+//alert("hello");
+let name="Ammar";
+let age=18;
+let isloggedIn= false;
+
+//bigint= we this due to having biggest number
+//string==>""
+//boolean==> true /false
+//null==> it is standalone value 
+//undefined==> i havn't assigned value but assiged varaible
+//symbol==>to find the uniqueness we use this
+
+//object
+console.log(typeof "Ammar");
+console.log(typeof null);//it tell the type of thing is that string??number?
+
