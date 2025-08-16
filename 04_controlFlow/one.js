9fd363f7a04c53d652f14b8d59361c6314785801
ammaraltaf064 <ammaraltaf064@gmail.com>
2025-08-16 09:48:14 +0000

    if useD and comparison

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,49 @@
+//if
+
+if( 2 == "2"){
+    
+    // console.log("executed");
+    
+}
+
+//=== it chevks also the datatype(for strict checking)
+// const temperature=49;
+// if(temperature<47)    
+// {
+//     console.log("less than 50");
+// }
+// else{
+    //     const power="fly";
+    //     console.log(`the power is: ${power}`);
+    
+    // }
+    
+    //console.log(`the power is: ${power}` it shows error
+
+const balance=1000;
+
+// if(balance > 500) console.log("test"),console.log("test2");//this is implicite function don't use this
+
+// if(balance < 500)
+// {
+//     console.log("less than 500");
+// }
+// else if(balance <900)
+// {
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("not satisfied");    
+// }
+
+const isUserLoggedIn= true;
+const debitcard =true;
+
+//&&--> And    ||--->OR
+
+if(isUserLoggedIn && debitcard && 2==3)
+{
+    console.log("allow to buy course");
+}
+
+
