083d41df3a22302659f5cb9ca15b4aef1496cbe9
ammaraltaf064 <ammaraltaf064@gmail.com>
2025-08-16 10:00:03 +0000

    switch

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,16 @@
+const month =4
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("jan");
+        break;
+    case 3:
+        console.log("jan");
+        break;
+
+    default:
+        console.log("nothing");
+        break;
+}
